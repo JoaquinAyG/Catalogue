5e4d49c109ffdb43f6425be883e6f45c738dc820
JoaquinAyG <joaquin.ayllon@educa.madrid.org>
2022-11-06 22:36:55 +0100

    csv reader

--- app/src/main/java/com/study/catalogue/MainActivity.java
+++ app/src/main/java/com/study/catalogue/MainActivity.java
@@ -1,14 +1,32 @@
 package com.study.catalogue;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.widget.TextView;
+
+import com.study.catalogue.models.Film;
+import com.study.catalogue.service.FilmLoader;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+
+    RecyclerView recyclerView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        FilmLoader filmLoader = new FilmLoader(this);
+        List<Film> filmList = new ArrayList<>(filmLoader.load());
+
+        recyclerView = findViewById(R.id.recyclerview);
+
+
+
     }
 }
---
+++ app/src/main/java/com/study/catalogue/service/FilmLoader.java
@@ -0,0 +1,53 @@
+package com.study.catalogue.service;
+
+import android.content.Context;
+
+import com.study.catalogue.models.Film;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+
+public class FilmLoader {
+
+    Context context;
+
+    public FilmLoader(Context context) {
+        this.context = context;
+    }
+
+    public List<Film> load() {
+        List<Film> filmList = new ArrayList<>();
+        try{
+            InputStream inputStream = context.getAssets().open("DisneyMoviesDataset.csv");
+            InputStreamReader inputStreamReader = new InputStreamReader(inputStream);
+            BufferedReader reader = new BufferedReader(inputStreamReader);
+            String line;
+            reader.readLine();
+            while ((line = reader.readLine()) != null) {
+                filmList.add(parseFilm(line));
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return filmList;
+    }
+
+    private Film parseFilm(String line) {
+        String[] fields = line.split(",");
+        int id = (int)Float.parseFloat(fields[0]);
+        String title = fields[1];
+        int runningTime = (int)Float.parseFloat(fields[2]);
+        float budget = Float.parseFloat(fields[3]);
+        float boxOffice = Float.parseFloat(fields[4]);
+        String releaseDate2 = fields[5];
+        String directedBy = fields[6];
+        String producedBy = fields[8];
+        String musicBy = fields[9];
+        return new Film(id, title, runningTime, budget, boxOffice, releaseDate2, directedBy, producedBy, musicBy);
+    }
+
+}
