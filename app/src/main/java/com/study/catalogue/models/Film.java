1af282941c06dd978f27b524f2b313a68cadd8c7
JoaquinAyG <joaquin.ayllon@educa.madrid.org>
2022-11-06 17:36:22 +0100

    initial commit

---
+++ app/src/main/java/com/study/catalogue/models/Film.java
@@ -0,0 +1,15 @@
+package com.study.catalogue.models;
+
+public class Film {
+
+    //Title,Release date,Running time (int),Budget (float),Box office (float),Release date (datetime),Directed by,Produced by,Music by
+    String title;
+    String releaseDate;
+    int runningTime;
+    float budget;
+    float boxOffice;
+    String releaseDate2;
+
+
+
+}
